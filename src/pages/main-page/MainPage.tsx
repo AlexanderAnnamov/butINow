9f89809ec1149692ea622ff23ab5c9cbf3a7c028
Александр Аннамов <hash_nam@mail.ru>
2024-08-07 19:43:32 +0500

    fix .env params

--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -1,29 +1,23 @@
 import { Center, Text } from '@chakra-ui/react'
 import { useEffect, useState } from 'react'
 
-import styles from './MainPage.module.css'
-
-// const KEY_NEURO_SERVICE = process.env.REACT_APP_API_KEY
+const KEY = import.meta.env.VITE_APP_KEY_GEN_API;
 
 const MainPage = () => {
-    const [statusRequest, setStatusRequest] = useState<object | null | undefined>()
+    const [statusRequest, setStatusRequest] = useState<{request_id: string} | null | undefined>()
     const [textMessage, setTextMessage] = useState<string | null>()
-
-    console.log(textMessage)
-
     const checkTask = () => {
         fetch("https://api.gen-api.ru/api/v1/networks/chat-gpt-3", { method: "POST",
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
-                'Authorization': `Bearer ${KEY_NEURO_SERVICE}`
+                'Authorization': `Bearer ${KEY}`
             },
             body: JSON.stringify({messages: [{role: "system", content: 'сгенерируй любую интересную шутку'}], response_format: "{\"type\":\"text\"}"})
           })
         .then(response => response.json())
         .then(data => setStatusRequest(data))
         .catch(error => console.error("Ошибка:", error));
-       
     }
     useEffect(() => {
         setTimeout(() => {
@@ -33,7 +27,7 @@ const MainPage = () => {
                     headers: {
                         'Content-Type': 'application/json',
                         'Accept': 'application/json',
-                        'Authorization': `Bearer ${KEY_NEURO_SERVICE}`
+                        'Authorization': `Bearer ${KEY}`
                     },
                   }).then(response => response.json()).then((data) => {
                     if(data.status === 'success') {
@@ -46,7 +40,6 @@ const MainPage = () => {
 
   return (
     <Center bg='black' h='100vh' color='white'>
-        <div className={styles.circle}></div>
         <Text zIndex={99} maxWidth={'60vw'} textAlign={'right'} onClick={checkTask} fontSize='3vw'>{textMessage || 'Тапни ка бы я подумал'}</Text>
     </Center>
   )
---
+++ vite-env.d.ts
@@ -0,0 +1,7 @@
+interface ImportMetaEnv {
+    readonly KEY_GEN_API: string;   
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
